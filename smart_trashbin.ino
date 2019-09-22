3581b7affb92ff0e344f52233350869bab540885
nkkusumax <32930030+nkkusumax@users.noreply.github.com>
2019-09-22 15:23:57 +0800

    Add files via upload

---
+++ smart_trashbin.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+int obstacle = 2; // This is our input pin
+int obstacle_stts = HIGH; // HIGH MEANS CLEAR
+Servo myservo;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(obstacle,INPUT);
+  Serial.begin(9600);
+  myservo.attach(3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  obstacle_stts = digitalRead(obstacle);
+  int x = map (obstacle_stts, 0, 1, 100, 0);
+  if (obstacle_stts == 0)
+  {
+    Serial.println("object detected !");
+    myservo.write(x);
+    delay(500);
+    }
+    else{
+      Serial.println("clear");
+      myservo.write(x);
+      }
+}
